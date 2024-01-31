429ad34272a04f9cb3b8c67dba52222aee9289be
k2645 <k2645@naver.com>
2024-01-31 18:24:36 +0900

    refactor : 접근제어 수준 private으로 수정

--- PrographyAssignment/View/HomeViewController.swift
+++ PrographyAssignment/View/HomeViewController.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+final class HomeViewController: UIViewController {
+    
+    private var pageNum = 0
+    private var photos: [Photo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,10 +19,9 @@ class HomeViewController: UIViewController {
         setNavigationBarBackground()
     }
 
-
 }
 
-extension HomeViewController {
+private extension HomeViewController {
     
     func setUI() {
         view.backgroundColor = .white
@@ -30,5 +32,19 @@ extension HomeViewController {
         appearance.configureWithOpaqueBackground()
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
+    
+//    func fetchPhoto() {
+//        UnsplashAPI.fetchPhotos(pageNum: pageNum + 1) { data, response, error in
+//            
+//            guard let _ = response as? HTTPURLResponse,
+//                  let data = data else { return }
+//            do {
+//                let fetchedPhotos = try JSONDecoder().decode([Photo].self, from: data)
+//                print(fetchedPhotos)
+//            } catch {
+//                print(error)
+//            }
+//        }
+//    }
     
 }
